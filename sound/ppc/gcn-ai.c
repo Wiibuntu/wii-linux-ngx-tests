f906414f1bd0e2c1c093f6e6368fa3c2a3e6008d
techflashYT <officialTechflashYT@gmail.com>
2024-07-07 04:08:14 -0700

    sound: ppc: small fixes for gcn-ai

--- sound/ppc/gcn-ai.c
+++ sound/ppc/gcn-ai.c
@@ -489,8 +489,8 @@ static int ai_do_probe(struct device *dev,
 	struct snd_gcn *chip;
 	int retval;
 
-	printk("gcn-ai: in ai_do_probe");
-	retval = snd_card_new(NULL, index, id, THIS_MODULE, sizeof(struct snd_gcn), &card);
+	printk("gcn-ai: in ai_do_probe\n");
+	retval = snd_card_new(dev, index, id, THIS_MODULE, sizeof(struct snd_gcn), &card);
 	if (retval < 0) {
 		drv_printk(KERN_ERR, "failed to allocate card\n");
 		return -ENOMEM;
@@ -532,7 +532,7 @@ static int ai_of_probe(struct platform_device *odev)
 	struct resource dsp, ai;
 	int retval;
 
-	printk("gcn-ai: in ai_of_probe");
+	printk("gcn-ai: in ai_of_probe\n");
 	retval = of_address_to_resource(odev->dev.of_node, 0, &dsp);
 	if (retval) {
 		drv_printk(KERN_ERR, "no dsp io memory range found\n");
