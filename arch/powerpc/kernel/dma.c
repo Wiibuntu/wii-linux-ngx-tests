f20300074f765ac898f86ca17fa9650778a94bcc
Wiibuntu <146908244+Wiibuntu@users.noreply.github.com>
2024-12-05 18:19:54 -0500

    Fixes

--- arch/powerpc/kernel/dma.c
+++ arch/powerpc/kernel/dma.c
@@ -62,11 +62,6 @@ static int dma_direct_dma_supported(struct device *dev, u64 mask)
 #endif
 }
 
-void *__dma_direct_alloc_coherent(struct device *dev, size_t size,
-				  dma_addr_t *dma_handle, gfp_t flag,
-				  unsigned long attrs)
-{
-	void *ret;
 
 	/*
 	 * This code should be OK on other platforms, but we have drivers that
@@ -248,7 +243,22 @@ static inline void dma_direct_unmap_page(struct device *dev,
 static inline void dma_direct_sync_sg(struct device *dev,
 		struct scatterlist *sgl, int nents,
 		enum dma_data_direction direction)
-{
+{#ifdef CONFIG_NOT_COHERENT_CACHE
+	if (dma_alloc_from_coherent(dev, size, dma_handle, &ret))
+		return ret;
+
+	ret = __dma_alloc_coherent(dev, size, dma_handle, flag);
+	if (ret == NULL)
+		return NULL;
+	*dma_handle += get_dma_offset(dev);
+	return ret;
+#else
+	struct page *page;
+	int node = dev_to_node(dev);
+#ifdef CONFIG_FSL_SOC
+	u64 pfn = get_pfn_limit(dev);
+	int zone;
+
 	struct scatterlist *sg;
 	int i;
 
